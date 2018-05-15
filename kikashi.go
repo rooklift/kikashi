3aae68f411ad79a40e91e48bc97a0f01bf29b155
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-05-15 11:23:28 +0100

    renderer

--- kikashi.go
+++ kikashi.go
@@ -7,9 +7,11 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+const TITLE = "Kikashi"
+
 type App struct {
 	Window			*sdl.Window
-	Surface			*sdl.Surface
+	Renderer		*sdl.Renderer
 	Width			int32
 	Height			int32
 }
@@ -28,14 +30,14 @@ func (self *App) Init(width, height int32) {
 
 	fmt.Printf("CreateWindow...\n")
 
-	self.Window, err = sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, width, height, sdl.WINDOW_SHOWN)
+	self.Window, err = sdl.CreateWindow(TITLE, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, width, height, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("GetSurface...\n")
+	fmt.Printf("CreateRenderer...\n")
 
-	self.Surface, err = self.Window.GetSurface()
+	self.Renderer, err = sdl.CreateRenderer(self.Window, -1, sdl.RENDERER_ACCELERATED)
 	if err != nil {
 		panic(err)
 	}
@@ -43,13 +45,15 @@ func (self *App) Init(width, height int32) {
 
 func (self *App) Shutdown() {
 	fmt.Printf("Destroy...\n")
+	self.Renderer.Destroy()
+	fmt.Printf("Destroy...\n")
 	self.Window.Destroy()
 	fmt.Printf("Quit...\n")
 	sdl.Quit()
 }
 
 func (self *App) Flip() {
-	self.Window.UpdateSurface()
+	self.Renderer.Present()
 }
 
 func (self *App) Poll() {
@@ -62,9 +66,9 @@ func (self *App) Poll() {
 	}
 }
 
-func (self *App) Cls(r, g, b uint32) {
-	argb := 0xff000000 + (r % 256) << 16 + (g % 256) << 8 + b
-	self.Surface.FillRect(&sdl.Rect{0, 0, self.Width, self.Height}, argb)
+func (self *App) Cls(r, g, b uint8) {
+	self.Renderer.SetDrawColor(r, g, b, 255)
+	self.Renderer.FillRect(&sdl.Rect{0, 0, self.Width, self.Height})
 }
 
 func main() {
